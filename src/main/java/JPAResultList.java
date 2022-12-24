5423caa284a76f5b1c39bc565875ced44cfaf7bb
CPL <carlos@centropyme.com>
2022-12-24 02:07:51 +0100

    En este commit se ha añadido en JPAResultList la opción de listar todos los perfiles introduciendo un "*"

--- src/main/java/JPAResultList.java
+++ src/main/java/JPAResultList.java
@@ -1,23 +1,31 @@
 import Entidades.Usuario;
 import Utilidades.JPAem;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 
 import javax.swing.*;
 import java.util.List;
 
 public class JPAResultList {
     public static void main(String[] args) {
+        // Parte fija de la query
+        String qry = "select u from Usuario u";
+
         String perfil = JOptionPane.showInputDialog("Indique el perfil a buscar");
-        // Abrimos la conexión
-        EntityManager em = JPAem.getEntityManager();
-        /* Creamos la consulta de tipo Query
+        /* Parte variable de la query
            La partícula ?1 indica que es un dato que se proporciona como parámetro:
                - el 1 indica que es el primer (y en este caso único) parámetro.
                - Si hubiera más, se indicarían con ?2, ?3 ...
          */
-        List<Usuario> listaUsu  = em.createQuery("select u from Usuario u where u.perfil=?1", Usuario.class)
-                .setParameter(1, perfil)    // el uno indica el ordinal del parámetro proporcionado
-                .getResultList();
+        qry += (perfil.equals("*")) ? "" : " where u.perfil=?1";
+        // Abrimos la conexión
+        EntityManager em = JPAem.getEntityManager();
+
+        Query qryUsu  = em.createQuery(qry, Usuario.class);
+        if (!perfil.equals("*")) {
+            qryUsu.setParameter(1, perfil); // el uno indica el ordinal del parámetro proporcionado
+        }
+        List<Usuario> listaUsu = qryUsu.getResultList();
 
         if (listaUsu.size() > 0) {
             listaUsu.forEach(System.out::println);
