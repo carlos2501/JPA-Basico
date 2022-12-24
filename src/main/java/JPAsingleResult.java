a9a0c14e26011294a8eb6bc37e11bee7ec3e9b6e
CPL <carlos@centropyme.com>
2022-12-24 01:13:56 +0100

    En esta rama vamos a probar la función getSingleResult() con una consulta. Veremos que cuando se devuelve más de un registro, se genera un error, que podremos evitar utilizando setMaxResult(). Si esperamos obtener un solo registro, está función es más rápida.

---
+++ src/main/java/JPAsingleResult.java
@@ -0,0 +1,31 @@
+import Entidades.Usuario;
+import Utilidades.JPAem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+
+import javax.swing.*;
+
+public class JPAsingleResult {
+    public static void main(String[] args) {
+        String perfil = JOptionPane.showInputDialog("Indique el perfil a buscar");
+        // Abrimos la conexión
+        EntityManager em = JPAem.getEntityManager();
+        /* Creamos la consulta de tipo Query
+            La partícula ?1 indica que es un dato que se proporciona como parámetro:
+                - el 1 indica que es el primer (y en este caso único) parámetro.
+                - Si hubiera más, se indicarían con ?2, ?3 ...
+         */
+        Query qry = em.createQuery("select u from Usuario u where u.perfil=?1", Usuario.class);
+        qry.setParameter(1, perfil);    // el uno indica el ordinal del parámetro proporcionado
+        /*  La siguiente sentencia funciona con "admin" porque solo hay un registro con ese perfil, pero
+            no funciona con "usuario" porque hay más de un registro con ese perfil y devuelve el error
+            NonUniqueResultException: query did not return a unique result
+         */
+        // Si no utilizamos el cast -(Usuario)- no funciona porque getSingleResult() devuelve un objeto genérico
+        Usuario usu = (Usuario) qry.getSingleResult();
+        System.out.println(usu);
+
+        // FUNDAMENTAL. No olvidar
+        em.close();
+    }
+}
